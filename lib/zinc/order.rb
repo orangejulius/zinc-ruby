6f467b8e980fc57bf0c659def5380b8e793d8a68
Julian Simioni <julian.simioni@gmail.com>
2013-02-13 12:15:33 -0800

    Use method_missing to return values from internal hash in Order object

--- lib/zinc/order.rb
+++ lib/zinc/order.rb
@@ -1,14 +1,31 @@
 module Zinc
 	class Order
 		def self.create(params = {})
+			o = Order.new
 			if params != {}
 				response = Zinc.request(:post, url, params)
+				o.set_values(response.body)
 			end
-			Order.new
+			o
 		end
 
 		def self.url
 			Zinc.url_base+self.name.split('::')[-1].downcase+'/'
 		end
+
+		def set_values(values)
+			@values = values
+		end
+
+		def method_missing(name, *args)
+			@values ||= {}
+			if name.to_s.end_with?('=')
+				@values[name] = args[0]
+			else
+				@values[name]
+			end
+		end
+
+		private
 	end
 end
