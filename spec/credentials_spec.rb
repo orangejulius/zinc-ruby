10c98bdfabea074f63f630cf63dbe397629e881d
Julian Simioni <julian.simioni@gmail.com>
2013-02-13 12:26:27 -0800

    Enable check that request with no api key throw exception

--- spec/credentials_spec.rb
+++ spec/credentials_spec.rb
@@ -4,7 +4,6 @@ module Zinc
 	describe Zinc do
 		context "credentials" do
 			it "throws an error if request made without api credentials" do
-				pending "order functionality"
 				Zinc.api_key = nil
 				expect { Zinc::Order.create }.to raise_error(Zinc::AuthenticationError)
 			end
