c11ddf0ae6cdf8df7676aff33bcc69e966478e10
Julian Simioni <julian.simioni@gmail.com>
2013-02-13 11:15:47 -0800

    Return API URL base from Zinc module

--- lib/zinc.rb
+++ lib/zinc.rb
@@ -1,11 +1,17 @@
 require 'zinc/order'
 
 module Zinc
+	@@url_base = 'https://api.zinc.io/v1/'
+
 	def self.api_key=(key)
 		@@api_key = key
 	end
 
 	def self.api_key
 		@@api_key
+	end
+
+	def self.url_base
+		@@url_base
 	end
 end
